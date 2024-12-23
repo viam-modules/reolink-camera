820271ae59f20eb25d9b325d9b7315c0e24ec0ff
Steve Briskin <steve@viam.com>
2024-12-23 18:52:22 -0500

    added ptz commands

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ from viam.module.module import Module
 from viam.proto.app.robot import ComponentConfig
 from viam.proto.common import ResourceName, ResponseMetadata
 from viam.proto.component.camera import GetPropertiesResponse
+from viam.components.component_base import ValueTypes
 from viam.resource.base import ResourceBase
 from viam.resource.easy_resource import EasyResource
 from viam.resource.types import Model, ModelFamily
@@ -24,7 +25,7 @@ from viam.resource.registry import Registry, ResourceCreatorRegistration
 
 class Reolink(Camera, EasyResource):
     MODEL: ClassVar[Model] = Model(
-        ModelFamily("stevebriskin", "reolink-camera"), "reolink"
+        ModelFamily("viam", "camera"), "reolink"
     )
     LOGGER: Final[logging.Logger] = logging.getLogger(__name__)
     
@@ -115,6 +116,104 @@ class Reolink(Camera, EasyResource):
     ) -> Tuple[List[NamedImage], ResponseMetadata]:
         image = await self.get_image()
         return [NamedImage("main", image.data, image.mime_type)], ResponseMetadata()
+
+    # Exposes select commands from Reolink API as documented at https://drive.google.com/file/d/1KvPbjRVqsgCEzJsUS--zEyxw6cP-pkSI/view?usp=sharing.
+    async def do_command(
+        self,
+        command: Mapping[str, ValueTypes],
+        *,
+        timeout: Optional[float] = None,
+        **kwargs
+    ) -> Dict[str, Any]:
+
+        result = {}
+
+        for cmd_name, cmd_args in command.items():
+            result[cmd_name] = {}
+
+            self.logger.warning(f"command: {cmd_name}, args: {cmd_args}, type: {type(cmd_args)}")
+
+            # TODO: figure out how to parse a map out of command
+            parsed_args = {}
+
+            match cmd_name:
+                # Movement. From https://github.com/ReolinkCameraAPI/reolinkapipy/blob/master/reolinkapi/mixins/ptz.py
+                case "ptz_move_right":
+                    ret = self.camera.move_right()
+                    result[cmd_name]["result"] = ret
+                case "ptz_move_left":
+                    ret = self.camera.move_left()
+                    result[cmd_name]["result"] = ret
+                case "ptz_stop":
+                    ret = self.camera.stop_ptz()
+                    result[cmd_name]["result"] = ret
+                case "ptz_move_up":
+                    ret = self.camera.move_up()
+                    result[cmd_name]["result"] = ret
+                case "ptz_move_down":
+                    ret = self.camera.move_down()
+                    result[cmd_name]["result"] = ret
+                case "ptz_move_right_up":
+                    ret = self.camera.move_right_up()
+                    result[cmd_name]["result"] = ret
+                case "ptz_move_right_down":
+                    ret = self.camera.move_right_down()
+                    result[cmd_name]["result"] = ret
+                case "ptz_move_left_up":
+                    ret = self.camera.move_left_up()
+                    result[cmd_name]["result"] = ret
+                case "ptz_move_left_down":
+                    ret = self.camera.move_left_down()
+                    result[cmd_name]["result"] = ret
+                case "ptz_go_to_preset":
+                    ret = self.camera.go_to_preset()
+                    result[cmd_name]["result"] = ret
+                case "ptz_add_preset":
+                    ret = self.camera.add_preset()
+                    result[cmd_name]["result"] = ret
+                case "ptz_remove_preset":
+                    ret = self.camera.remove_preset()
+                    result[cmd_name]["result"] = ret
+                case "ptz_perform_calibration":
+                    ret = self.camera.perform_calibration()
+                    result[cmd_name]["result"] = ret
+                case "ptz_get_presets":
+                    ret = self.camera.get_ptz_presets()
+                    result[cmd_name]["result"] = ret
+                case "ptz_get_check_state":
+                    ret = self.camera.get_ptz_check_state()
+                    result[cmd_name]["result"] = ret
+                case "ptz_auto_movement":
+                    ret = self.camera.auto_movement()
+                    result[cmd_name]["result"] = ret
+
+                # Zoom and Focus. From https://github.com/ReolinkCameraAPI/reolinkapipy/blob/master/reolinkapi/mixins/zoom.py
+                case "ptz_start_zooming_in":
+                    ret = self.camera.start_zooming_in()
+                    result[cmd_name]["result"] = ret
+                case "ptz_start_zooming_out":
+                    ret = self.camera.start_zooming_out()
+                    result[cmd_name]["result"] = ret
+                case "ptz_stop_zooming":
+                    ret = self.camera.stop_zooming()
+                    result[cmd_name]["result"] = ret
+                case "ptz_start_focusing_in":
+                    ret = self.camera.start_focusing_in()
+                    result[cmd_name]["result"] = ret
+                case "ptz_start_focusing_out":
+                    ret = self.camera.start_focusing_out()
+                    result[cmd_name]["result"] = ret
+                case "ptz_stop_focusing":
+                    ret = self.camera.stop_focusing()
+                    result[cmd_name]["result"] = ret
+
+
+                # TODO: add move, zoom, focus to specific values commands after they're added to the python api.
+
+                case _:
+                    result["error"] = f"Unknown command: {command}"
+
+        return result
 
     async def get_point_cloud(
         self,
