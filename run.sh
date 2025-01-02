016ffa0cc161a92e009ecc69e52a2318a0970546
Steve Briskin <steve@viam.com>
2025-01-02 12:18:28 -0500

    python build fun

--- run.sh
+++ run.sh
@@ -2,9 +2,7 @@
 
 cd `dirname $0`
 
-uv venv --python 3.12.8
 source .venv/bin/activate
-python3 -m pip install -r requirements.txt -U 
 
 # Be sure to use `exec` so that termination signals reach the python process
 exec python3 src/main.py $@
